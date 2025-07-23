50df8856b437d1ec3a76551a3eccbb55713d4008
gingerlavender <vkurnosoff76@gmail.com>
2025-07-24 01:55:59 +0300

    Initial Foundry setup
    * Basic project structure
    * OZ and Chainlink dependencies
    * Solhint

---
+++ script/LotteryDeploy.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {Lottery} from "src/Lottery.sol";
+
+contract LotteryDeployScript is Script {
+    function setUp() external {}
+
+    function run() external {}
+}
