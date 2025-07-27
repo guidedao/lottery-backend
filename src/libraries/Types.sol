eaaae7c8732f9af83bd2c1a0793898e729693654
gingerlavender <vkurnosoff76@gmail.com>
2025-07-27 07:06:37 +0300

    Fixed LotteryStatus documentation

--- src/libraries/Types.sol
+++ src/libraries/Types.sol
@@ -7,32 +7,22 @@ pragma solidity ^0.8.30;
 library Types {
     /**
      * @notice Enum values describing current lottery status.
+     * @dev Statuses desription:
+     * - Closed: there is no active lottery event at the moment.
+     * - OpenedForRegistration: users can enter and quit the lottery only during this phase.
+     * - RegistrationEnded: registration time has ended, but winner reveal is not pending yet.
+     * That actually means that registration is closed, but Chainlink VRF
+     * {requestRandomWords} has not been called yet.
+     * - WaitingForReveal: that means that {requestRandomWords} is called,
+     * and we are waiting for the oracle response.
+     * - Invalid: Lottery is invalid as there were not enough
+     * participants after registration.
      */
     enum LotteryStatus {
-        /**
-         * @notice There is no active lottery event at the moment.
-         */
         Closed,
-        /**
-         * @notice Users can enter and quit the lottery only during this phase.
-         */
         OpenedForRegistration,
-        /**
-         * @notice Registration time has ended, but winner reveal is not pending yet.
-         * @dev That actually means that registration is closed, but Chainlink VRF
-         * {requestRandomWords} has not been called yet.
-         */
         RegistrationEnded,
-        /**
-         * @notice Waiting for winner reveal.
-         * @dev That means that {requestRandomWords} is called,
-         * and we are waiting for the oracle response.
-         */
         WaitingForReveal,
-        /**
-         * @notice Lottery is invalid as there were not enough
-         * participants after registration.
-         */
         Invalid
     }
 }
