1713ece99c231811bb1993b36979553820f78d9c
gingerlavender <vkurnosoff76@gmail.com>
2025-08-02 02:35:50 +0300

    Started implementing contract functionality.
    * Added basic structures:
    - ParticipantInfo
    - RefundBatch
    - LotteryState
    
    * Made draft for some constants with dummy values (business logic related variables, old lottery data cleanup limitations)
    
    * Organized the way participants are stored (participants, participantsInfo)
    * Organized the way refund batches are stored and tied to former participants
    
    * Implemented most of the 'lottery flow operator' functions
    * Implemented enter() function
    * Implemented refund logic
    * Implemented lottery status derivation
    * Implemented possibility to clear old data about former lottery participants (for lotteries that were held a defined constant number of iterations ago)

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -13,72 +13,508 @@ import {Types} from "./libraries/Types.sol";
  */
 contract Lottery is ILottery, ILotteryErrors, VRFConsumerBaseV2Plus {
     /**
-     * @dev A set of state variables used
-     * to derive current lottery status and defining its lifecycle.
+     * @notice Information about every lottery participant.
+     */
+    struct ParticipantInfo {
+        bool isParticipant;
+        bytes encryptedContactDetails;
+    }
+
+    /**
+     * @notice Common information about all the refunds after
+     * particular invalid lottery.
+     * @dev Important to notice that `unclaimedRefundsNumber` decrements
+     * after every user refund, so if whole refund batch expired,
+     * organizer can simply withdraw `unclaimedRefundsNumber` * `ticketPrice` (which
+     * can be different across batches!) wei.
+     */
+    struct RefundBatch {
+        uint256 refundAssignmentTime;
+        uint256 ticketPrice;
+        uint256 unclaimedRefundsNumber;
+    }
+
+    /**
+     * @dev A set of state variables used to get information about
+     * current lottery event and derive its status.
      */
     struct LotteryState {
         uint256 registrationEndTime;
+        uint256 totalExtensionTime;
+        bool wasStarted;
         bool waitingForOracleResponse;
-        // To be done
+        uint256 participantsCount;
     }
+
+    /* dummy values for now */
+    uint256 public constant TARGET_PARTICIPANTS_NUMBER = 30;
+    uint256 public constant REGISTRATION_DURATION = 21 days;
+    uint256 public constant MAX_EXTENSION_TIME = 7 days;
+    uint256 public constant REFUND_WINDOW = 14 days;
+
+    /**
+     * @notice Number of lotteries that must pass after a certain one
+     * in order to be able to clear its data.
+     */
+    uint8 public constant LOTTERY_DATA_FRESHNESS_INTERVAL = 5;
+    /**
+     * @notice Maximum amount of participants that can be cleared
+     * in a single {clearLotteryData} call.
+     */
+    uint8 public constant MAX_PARTICIPANTS_TO_CLEAR = 40;
+
+    /* Chainlink VRF configuration (see VRFConsumerConfig in
+     libraries/Configs.sol) */
+    uint256 private immutable SUBSCRIPTION_ID;
+    bytes32 private immutable KEY_HASH;
+    uint32 private immutable CALLBACK_GAS_LIMIT;
+    uint16 private immutable REQUEST_CONFIRMATIONS;
+
+    address public immutable GUIDE_DAO_TOKEN;
+
+    /**
+     * @dev Address that can receive money from lotteries and
+     * expired refunds.
+     */
+    address private _organizer;
 
     LotteryState private _state;
 
-    uint256 private immutable SUBSCRIPTION_ID;
-    bytes32 private immutable KEY_HASH;
-    uint32 private immutable CALLBACK_GAS_LIMITS;
-    uint16 private immutable REQUEST_CONFIRMATIONS;
-    uint32 private immutable NUM_WORDS;
+    /**
+     * @notice Returns current lottery number.
+     * If no one was started up to this moment, returns 0.
+     */
+    uint256 public lotteryNumber;
 
-    address public immutable _guideDAOToken;
+    /**
+     * @notice Returns participant address from particular lottery by its number
+     * and participant index.
+     */
+    mapping(uint256 lotteryNumber => mapping(uint256 index => address))
+        public participants;
+
+    /**
+     * @notice Returns participant information from particular lottery by its number
+     * and participant index.
+     */
+    mapping(uint256 lotteryNumber => mapping(address user => ParticipantInfo)) participantsInfo;
+
+    /**
+     * @notice Returns refund batch by its id.
+     * Important to notice that there is always one refund batch per
+     * one invalid lottery.
+     */
+    mapping(uint256 batchId => RefundBatch) public refundBatches;
+
+    /**
+     * @notice Returns batches for which specific user can
+     * get their money back.
+     */
+    mapping(address participant => uint256[]) public refundBatchIds;
+
+    /**
+     * @notice If current lottery is declared invalid,
+     * it will produce refund batch with the corresponding id.
+     *
+     * Returns 0 if there is no refund batches avialable.
+     */
+    uint256 public nextRefundBatchId;
+
+    /**
+     * @notice Funds that organizer can freely withdraw to their address.
+     * @dev Updated in {fulfillRandomWords}.
+     */
+    uint256 public organizerFunds;
+
+    uint256 public ticketPrice;
 
     address public lastWinner;
 
+    /**
+     * @notice Data about participants of lottery with `lotteryNumber` number (specifically
+     * participants and corresponding participantsInfo) has been cleared from storage,
+     * from one index in {participants} to another.
+     */
+    event LotteryDataCleared(
+        uint256 lotteryNumber,
+        uint256 fromParticipantIndex,
+        uint256 toParticipantIndex
+    );
+
+    /**
+     * @notice Unable to clear lottery data because
+     * current lottery number is not sufficient.
+     */
+    error TooEarlyToClearData(
+        uint256 receivedCurrentLotteryNumber,
+        uint256 minimumCurrentLotteryNumber
+    );
+
+    /**
+     * @dev There is no data about participants of lottery
+     * with given number to clear at some index (either there was no data
+     * or it had already been cleared),
+     */
+    error NothingToClear(uint256 lotteryNumber, uint256 fromParticipantIndex);
+
     constructor(
-        address guideDAOToken,
+        address organizer,
+        uint256 _ticketPrice,
+        address _guideDAOToken,
         address _vrfCoordinator,
-        uint256 subscriptionId,
-        bytes32 keyHash,
-        uint32 callbackGasLimit,
-        uint16 requestConfirmations,
-        uint32 numWords
+        uint256 _subscriptionId,
+        bytes32 _keyHash,
+        uint32 _callbackGasLimit,
+        uint16 _requestConfirmations
     ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
-        _guideDAOToken = guideDAOToken;
-        SUBSCRIPTION_ID = subscriptionId;
-        KEY_HASH = keyHash;
-        CALLBACK_GAS_LIMITS = callbackGasLimit;
-        REQUEST_CONFIRMATIONS = requestConfirmations;
-        NUM_WORDS = numWords;
+        _organizer = organizer;
+        ticketPrice = _ticketPrice;
+        GUIDE_DAO_TOKEN = _guideDAOToken;
+        SUBSCRIPTION_ID = _subscriptionId;
+        KEY_HASH = _keyHash;
+        CALLBACK_GAS_LIMIT = _callbackGasLimit;
+        REQUEST_CONFIRMATIONS = _requestConfirmations;
     }
 
+    /**
+     * @inheritdoc ILottery
+     */
     function registrationEndTime() external view returns (uint256) {
         return _state.registrationEndTime;
     }
 
-    function status() external view returns (Types.LotteryStatus) {}
+    /**
+     * @inheritdoc ILottery
+     */
+    function status() external view returns (Types.LotteryStatus) {
+        return _status();
+    }
 
-    function enter(bytes calldata _encryptedContactDetails) external payable {}
+    /**
+     * @inheritdoc ILottery
+     */
+    function refundAmount(address _user) external view returns (uint256) {
+        uint256[] storage userRefundBatchIds = refundBatchIds[_user];
 
+        uint256 totalRefundAmount;
+
+        for (uint256 i = 0; i < userRefundBatchIds.length; i++) {
+            uint256 batchId = userRefundBatchIds[i];
+            RefundBatch storage batch = refundBatches[batchId];
+
+            if (block.timestamp <= batch.refundAssignmentTime + REFUND_WINDOW) {
+                totalRefundAmount += batch.ticketPrice;
+            }
+        }
+
+        return totalRefundAmount;
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function enter(bytes calldata _encryptedContactDetails) external payable {
+        Types.LotteryStatus currentStatus = _status();
+        require(
+            currentStatus == Types.LotteryStatus.OpenedForRegistration,
+            IncorrectLotteryStatus(
+                currentStatus,
+                Types.LotteryStatus.OpenedForRegistration
+            )
+        );
+
+        require(
+            !participantsInfo[lotteryNumber][msg.sender].isParticipant,
+            AlreadyRegistered(msg.sender)
+        );
+
+        require(
+            msg.value == ticketPrice,
+            InsufficientFunds(msg.sender, msg.value, ticketPrice)
+        );
+
+        participants[lotteryNumber][_state.participantsCount++] = msg.sender;
+
+        ParticipantInfo storage userInfo = participantsInfo[lotteryNumber][
+            msg.sender
+        ];
+
+        userInfo.encryptedContactDetails = _encryptedContactDetails;
+        userInfo.isParticipant = true;
+
+        emit ParticipantRegistered(lotteryNumber, msg.sender);
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
     function quit() external {}
 
-    function refund() external {}
+    /**
+     * @inheritdoc ILottery
+     */
+    function refund() external {
+        uint256[] storage userRefundBatchIds = refundBatchIds[msg.sender];
 
-    function start() external {}
+        uint256 totalRefundAmount;
 
-    function extendRegistrationTime(uint256 _duration) external {}
+        for (uint256 i = 0; i < userRefundBatchIds.length; i++) {
+            uint256 batchId = userRefundBatchIds[i];
+            RefundBatch storage batch = refundBatches[batchId];
 
-    function closeInvalidLottery() external {}
+            if (block.timestamp <= batch.refundAssignmentTime + REFUND_WINDOW) {
+                totalRefundAmount += batch.ticketPrice;
+                batch.unclaimedRefundsNumber--;
+            }
 
-    function requestWinner() external {}
+            if (batch.unclaimedRefundsNumber == 0) {
+                delete refundBatches[batchId];
+            }
+        }
 
-    function withdrawOrganizerFunds() external {}
+        delete refundBatchIds[msg.sender];
 
-    function collectExpiredRefunds() external {}
+        require(totalRefundAmount > 0, ZeroRefundBalance(msg.sender));
 
-    function ticketPrice() external view override returns (uint256) {}
+        (bool success, ) = msg.sender.call{value: totalRefundAmount}("");
 
+        require(success, WithdrawFailed(msg.sender));
+
+        emit MoneyRefunded(msg.sender, totalRefundAmount);
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function start() external /* access modifier */ {
+        Types.LotteryStatus currentStatus = _status();
+        require(
+            currentStatus == Types.LotteryStatus.Closed,
+            IncorrectLotteryStatus(currentStatus, Types.LotteryStatus.Closed)
+        );
+
+        lotteryNumber++;
+
+        _state.registrationEndTime = block.timestamp + REGISTRATION_DURATION;
+        _state.wasStarted = true;
+
+        emit LotteryStarted(lotteryNumber, block.timestamp);
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function extendRegistrationTime(
+        uint256 _duration
+    ) external /* access modifier */ {
+        Types.LotteryStatus currentStatus = _status();
+        require(
+            currentStatus == Types.LotteryStatus.OpenedForRegistration,
+            IncorrectLotteryStatus(
+                currentStatus,
+                Types.LotteryStatus.OpenedForRegistration
+            )
+        );
+
+        uint256 desiredExtensionTime = _state.totalExtensionTime + _duration;
+        require(
+            desiredExtensionTime <= MAX_EXTENSION_TIME,
+            ExtensionTooLong(desiredExtensionTime, MAX_EXTENSION_TIME)
+        );
+
+        _state.registrationEndTime += _duration;
+
+        emit RegistrationTimeExtended(lotteryNumber, _duration);
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function closeInvalidLottery() external /* access modifier */ {
+        Types.LotteryStatus currentStatus = _status();
+        require(
+            currentStatus == Types.LotteryStatus.Invalid,
+            IncorrectLotteryStatus(currentStatus, Types.LotteryStatus.Invalid)
+        );
+
+        uint256 currentBatchId = nextRefundBatchId++;
+
+        refundBatches[currentBatchId] = RefundBatch({
+            refundAssignmentTime: block.timestamp,
+            unclaimedRefundsNumber: _state.participantsCount,
+            ticketPrice: ticketPrice
+        });
+
+        for (uint256 i = 0; i < _state.participantsCount; i++) {
+            address participant = participants[lotteryNumber][i];
+            refundBatchIds[participant].push(currentBatchId);
+        }
+
+        delete _state;
+
+        emit InvalidLotteryClosed(lotteryNumber, block.timestamp);
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function requestWinner() external /* access modifier */ {
+
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function withdrawOrganizerFunds() external /* access modifier */ {
+        require(organizerFunds > 0, ZeroOrganizerBalance());
+
+        emit OrganizerFundsWithdrawn(organizerFunds);
+
+        organizerFunds = 0;
+
+        (bool success, ) = _organizer.call{value: organizerFunds}("");
+
+        require(success, WithdrawFailed(_organizer));
+    }
+
+    /**
+     * @inheritdoc ILottery
+     */
+    function collectExpiredRefunds(
+        uint256 _batchId
+    ) external /* access modifier */ {
+        RefundBatch storage batch = refundBatches[_batchId];
+
+        require(
+            batch.refundAssignmentTime + REFUND_WINDOW < block.timestamp &&
+                batch.unclaimedRefundsNumber > 0,
+            NoExpiredRefunds()
+        );
+
+        uint256 totalRefundAmount = batch.unclaimedRefundsNumber *
+            batch.ticketPrice;
+
+        delete refundBatches[_batchId];
+
+        (bool success, ) = _organizer.call{value: totalRefundAmount}("");
+
+        require(success, WithdrawFailed(_organizer));
+
+        emit ExpiredRefundsCollected(_batchId);
+    }
+
+    /**
+     * @notice Clear data about participants of given lottery,
+     * starting from one index to another with defined maximum
+     * of {MAX_PARTICIPANTS_TO_CLEAR}.
+     *
+     * Emits {LotteryDataCleared} event.
+     *
+     * Requirements:
+     * - Caller has permissions to clear lottery data
+     * - Difference between current lottery number and number of
+     * lottery to be cleared is at least {LOTTERY_DATA_FRESHNESS_INTERVAL}
+     * - There is some data to clear at the specified index.
+     */
+    function clearLotteryData(
+        uint256 _lotteryNumberToClear,
+        uint256 _fromParticipantIndex
+    ) external /* access modifier */ {
+        uint256 minimumCurrentLotteryNumber = _lotteryNumberToClear +
+            LOTTERY_DATA_FRESHNESS_INTERVAL;
+        require(
+            lotteryNumber >= minimumCurrentLotteryNumber,
+            TooEarlyToClearData(lotteryNumber, minimumCurrentLotteryNumber)
+        );
+
+        uint8 clearedAmount;
+
+        for (
+            uint i = _fromParticipantIndex;
+            clearedAmount < MAX_PARTICIPANTS_TO_CLEAR;
+            i++
+        ) {
+            address participant = participants[_lotteryNumberToClear][i];
+
+            if (participant == address(0)) {
+                break;
+            }
+
+            delete participantsInfo[_lotteryNumberToClear][participant];
+            delete participants[_lotteryNumberToClear][i];
+
+            clearedAmount++;
+        }
+
+        require(
+            clearedAmount > 0,
+            NothingToClear(_lotteryNumberToClear, _fromParticipantIndex)
+        );
+
+        emit LotteryDataCleared(
+            _lotteryNumberToClear,
+            _fromParticipantIndex,
+            _fromParticipantIndex + clearedAmount - 1
+        );
+    }
+
+    function setOrganizer(
+        address _newOrganizer
+    ) external /* access modifier */ {
+        require(_newOrganizer != address(0), ZeroOrganizerAddress());
+
+        emit OrganizerChanged(_organizer, _newOrganizer);
+        _organizer = _newOrganizer;
+    }
+
+    function setTicketPrice(
+        uint256 _newTicketPrice
+    ) external /* access modifier */ {
+        Types.LotteryStatus currentStatus = _status();
+        require(
+            currentStatus == Types.LotteryStatus.Closed,
+            IncorrectLotteryStatus(currentStatus, Types.LotteryStatus.Closed)
+        );
+
+        require(_newTicketPrice != 0, ZeroTicketPrice());
+
+        emit TicketPriceChanged(ticketPrice, _newTicketPrice);
+        ticketPrice = _newTicketPrice;
+    }
+
+    /**
+     * @dev Callback to receive random words from
+     * Chainlink oracle.
+     */
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
     ) internal virtual override {}
+
+    /**
+     * @dev Derives current lottery status.
+     *
+     *                  Lottery was started?
+     *                 / (Yes)         (No) \
+     *   Registration time has expired?   Closed
+     *        / (Yes)         (No) \
+     *   Enough participants?   OpenedForRegistration
+     *       / (Yes)   (No) \
+     *      Waiting for    Invalid
+     *    oracle response?
+     *     / (Yes)  (No) \
+     * WaitingForReveal  RegistrationEnded
+     */
+    function _status() private view returns (Types.LotteryStatus) {
+        if (!_state.wasStarted) return Types.LotteryStatus.Closed;
+        if (block.timestamp < _state.registrationEndTime)
+            return Types.LotteryStatus.OpenedForRegistration;
+        if (_state.participantsCount < TARGET_PARTICIPANTS_NUMBER)
+            return Types.LotteryStatus.Invalid;
+        if (_state.waitingForOracleResponse) {
+            return Types.LotteryStatus.WaitingForReveal;
+        } else {
+            return Types.LotteryStatus.RegistrationEnded;
+        }
+    }
 }
